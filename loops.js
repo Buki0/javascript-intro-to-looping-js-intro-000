1038a8758d6ea5bdc065d972fb8717e3b63e59e5
interpreted-network-8638 <muratibuki@gmail.com>
2020-03-11 02:10:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -27,16 +27,15 @@ function whileLoop(number)
 }
 
 
-function doWhileLoop(num)
-{
-  var i=0;
-  function incrementVariable() {
-    i++;
+function doWhileLoop(num) {
+	var i = 0;
+
+	function incrementVariable() {
+		i = i + 1;
     return i;
-  }
+	}
 
   do {
     console.log("I run once regardless.");
-  }
-  while (incrementVariable()<= num)
+  } while (incrementVariable() < num);
 }
